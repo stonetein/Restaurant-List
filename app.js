de7d474c499f0835185b5b2910a67d3275041de5
stone <stone.tein@gmail.com>
2024-04-23 14:22:45 +0800

    build route & controller setup

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
+
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('listing restaurants')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
+})
