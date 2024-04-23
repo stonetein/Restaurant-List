76f003c3221e789e295d1b057c7168ba9473e83a
stone <stone.tein@gmail.com>
2024-04-23 18:23:41 +0800

    feat: add search funtion to listing & fixed package.json

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
 
 app.use(express.static('public'))
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
 
 app.get('/', (req, res) => {
-  res.redirect('/restaurant')
+  res.redirect('/restaurants')
 })
 
-app.get('/restaurant', (req, res) => {
-  res.send('listing restaurant')
+app.get('/restaurants', (req, res) => {
+  const keyword = req.query.search?.trim()
+  console.log('keyword',keyword)
+  const matchedRes = keyword ? restaurants.filter((rt) =>
+    Object.values(rt).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : restaurants
+  res.render('index', { restaurants: matchedRes, keyword })
 })
 
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read restaurant: ${id}`)
+  const restaurant = restaurants.find((rt) => rt.id.toString() === id)
+  res.render('detail', {restaurant})
 })
+
